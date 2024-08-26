fe483f88e9959d267f7d8692e6c284aa69c14072
Kieran-Bayhack <kieran.bayhack@gmail.com>
2024-08-26 15:18:29 +0200

    Added validation for input

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
+    let regEx = /^pet_[A-Za-z0-9]+$/; //create variable to hold Regex - 
+    let input = document.getElementById('petInput').value; //create variable to store user input
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        if (regEx.test(input)) {
+            result = "Valid Syntax ✅";
+        }
+        else {
+            result = "Invalid syntax ❌";
+        }
 
             document.getElementById('result').innerText = result;
 }
